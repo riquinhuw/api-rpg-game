ccd41aea3df8c436b0750486c1b45e9b562bfa79
Carlos H <riquinhuw_lass@hotmail.com>
2021-05-11 00:32:39 -0300

    Ops, Heroku!

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/routes', (req, res) => {
     res.send(routes);
   })
 
-  console.log("Iniciando o Tibia Calc Damage. "+String(dia.getDate()).padStart(2, '0')+"/"+String(dia.getMonth() + 1).padStart(2, '0')+"/"+dia.getFullYear()+" "+dia.getHours()+":"+dia.getMinutes()+":"+dia.getSeconds());
+  console.log("Starting RPG GAME API. "+String(dia.getDate()).padStart(2, '0')+"/"+String(dia.getMonth() + 1).padStart(2, '0')+"/"+dia.getFullYear()+" "+dia.getHours()+":"+dia.getMinutes()+":"+dia.getSeconds());
 
 
   app.listen(process.env.PORT || 3333, ()=>{
