5e9bf91ed32055758801115ac2e0da27fb812560
Carlos H <riquinhuw_lass@hotmail.com>
2021-05-11 00:31:53 -0300

    Heroku :)

--- server.js
+++ server.js
@@ -12,4 +12,9 @@ app.get('/routes', (req, res) => {
     res.send(routes);
   })
 
-app.listen(3000);
+  console.log("Iniciando o Tibia Calc Damage. "+String(dia.getDate()).padStart(2, '0')+"/"+String(dia.getMonth() + 1).padStart(2, '0')+"/"+dia.getFullYear()+" "+dia.getHours()+":"+dia.getMinutes()+":"+dia.getSeconds());
+
+
+  app.listen(process.env.PORT || 3333, ()=>{
+    console.log('Listen in port 3333.')
+  });
